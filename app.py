f0a252ab4fd7953f6f02f20a112569c53d61deae
Taniya Thakur <thakurtanya633@gmail.com>
2026-03-27 13:24:16 +0530

    message

--- app.py
+++ app.py
@@ -117,3 +117,14 @@ def protect():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Flask is working 🚀"
+
+if __name__ == '__main__':
+    app.run(debug=True)
